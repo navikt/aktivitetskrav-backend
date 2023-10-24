e77a628430dcead82b045e081f3b1d802ac9f90f
Tor-Christian Tangenes <torchrct@gmail.com>
2023-10-24 12:21:10 +0200

    Bad SQL-grammar

--- src/main/resources/db/migration/V6__Create_indices_on_AKTIVITETSKRAV_tables.sql
+++ src/main/resources/db/migration/V6__Create_indices_on_AKTIVITETSKRAV_tables.sql
@@ -1,9 +1,9 @@
-CREATE INDEX aktivitetkrav_vurdering_index ON TABLE AKTIVITETSKRAV_VURDERING (
+CREATE INDEX aktivitetkrav_vurdering_index ON AKTIVITETSKRAV_VURDERING (
     person_ident,
     siste_vurdering_uuid
 );
 
-CREATE INDEX aktivitetkrav_varsel_index ON TABLE AKTIVITETSKRAV_VARSEL (
+CREATE INDEX aktivitetkrav_varsel_index ON AKTIVITETSKRAV_VARSEL (
     person_ident,
     vurdering_uuid
 );
