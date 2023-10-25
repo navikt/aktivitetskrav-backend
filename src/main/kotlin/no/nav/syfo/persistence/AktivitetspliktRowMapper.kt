eb9b6e8bfe984f7611bed2c3304872e8cd988c58
AudunSorheim <audun.sorheim@nav.no>
2023-10-25 15:06:50 +0200

    wip

--- src/main/kotlin/no/nav/syfo/persistence/AktivitetspliktRowMapper.kt
+++ src/main/kotlin/no/nav/syfo/persistence/AktivitetspliktRowMapper.kt
@@ -9,14 +9,14 @@ import java.sql.Timestamp
 class AktivitetspliktRowMapper : RowMapper<Aktivitetsplikt> {
     override fun mapRow(rs: ResultSet, rowNum: Int): Aktivitetsplikt {
         val status: String = rs.getString("status")
-        val arsaker: String = rs.getString("arsaker")
+        val arsaker: String? = rs.getString("arsaker").takeIf { it != "" }
         val sistVurdert: Timestamp? = rs.getTimestamp("sist_vurdert")
         val fristDato: Timestamp? = rs.getTimestamp("frist")
         val journalpostId: String? = rs.getString("journalpost_id")
 
         return Aktivitetsplikt(
             status = AktivitetspliktStatus.valueOf(status),
-            arsaker = arsaker.split(",").filter { it != "" },
+            arsaker = arsaker?.split(",") ?: emptyList(),
             sistVurdert = sistVurdert?.toZonedLocalDateTime(),
             fristDato = fristDato?.toZonedLocalDateTime(),
             journalpostId = journalpostId,
