48dfb0cb062cbec3e368ad7cd8e62139ee279503
Tor-Christian Tangenes <torchrct@gmail.com>
2023-10-25 07:53:13 +0200

    Replace log-statements with prometheus counters

---
+++ src/main/kotlin/no/nav/syfo/metric/Metric.kt
@@ -0,0 +1,29 @@
+package no.nav.syfo.metric
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class Metric @Autowired constructor(
+    private val registry: MeterRegistry
+) {
+    fun countKafkaErrorShutdown() = countEvent("shutdown_due_to_kafka_error")
+
+    fun countRecordReceived() = countEvent("kafka_record_received")
+
+    fun countAktivitetskravVurderingProcessed() = countEvent("aktivitetskrav_vurdering_processed")
+
+    fun countAktivitetskravVarselProcessed() = countEvent("aktivitetskrav_varsel_processed")
+
+    fun countEvent(name: String) {
+        registry.counter(
+            name,
+            Tags.of("type", "info")
+        ).increment()
+    }
+
+    private fun metricPrefix(name: String) =
+        "aktivitetskrav-backend_$name"
+}
--- src/main/kotlin/no/nav/syfo/service/AktivitetskravService.kt
+++ src/main/kotlin/no/nav/syfo/service/AktivitetskravService.kt
@@ -3,24 +3,26 @@ package no.nav.syfo.service
 import no.nav.syfo.kafka.consumer.domain.KAktivitetskravVarsel
 import no.nav.syfo.kafka.consumer.domain.KAktivitetskravVurdering
 import no.nav.syfo.logger
+import no.nav.syfo.metric.Metric
 import no.nav.syfo.persistence.AktivitetskravDAO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class AktivitetskravService @Autowired constructor(
-    private val aktivitetskravDAO: AktivitetskravDAO
+    private val aktivitetskravDAO: AktivitetskravDAO,
+    private val metric: Metric
 ) {
 
     private val log = logger()
 
     fun processAktivitetskravVurdering(vurdering: KAktivitetskravVurdering) {
-        log.info("Processing aktivitetskrav vurdering: ${vurdering.uuid}")
         aktivitetskravDAO.storeAktivitetkravVurdering(vurdering)
+        metric.countAktivitetskravVurderingProcessed()
     }
 
     fun processAktivitetskravVarsel(varsel: KAktivitetskravVarsel) {
-        log.info("Processing aktivitetskrav varsel: ${varsel.aktivitetskravUuid}")
         aktivitetskravDAO.storeAktivitetkravVarsel(varsel)
+        metric.countAktivitetskravVarselProcessed()
     }
 }
