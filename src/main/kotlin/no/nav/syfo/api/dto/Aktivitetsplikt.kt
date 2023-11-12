33394d0f10e2d06834151387494fb914e1f9a550
Tor-Christian Tangenes <torchrct@gmail.com>
2023-11-12 16:24:20 +0100

    Add 'LUKKET'-type to AktivitetspliktStatus-enum

--- src/main/kotlin/no/nav/syfo/api/dto/Aktivitetsplikt.kt
+++ src/main/kotlin/no/nav/syfo/api/dto/Aktivitetsplikt.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.api.dto
 import java.time.LocalDateTime
 
 enum class AktivitetspliktStatus {
-    NY, NY_VURDERING, AVVENT, UNNTAK, OPPFYLT, AUTOMATISK_OPPFYLT, FORHANDSVARSEL, IKKE_OPPFYLT, IKKE_AKTUELL
+    NY, NY_VURDERING, AVVENT, UNNTAK, OPPFYLT, AUTOMATISK_OPPFYLT, FORHANDSVARSEL, IKKE_OPPFYLT, IKKE_AKTUELL, LUKKET
 }
 
 data class Aktivitetsplikt(
