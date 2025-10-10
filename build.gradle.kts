e71df662f16263879d644601e41cb9159bdbfe61
André Wittwer <andre.wittwer@nav.no>
2025-10-10 15:05:05 +0200

    chore: bump kotlin and spring to 2.2.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "2.0.21"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.spring") version "2.2.20"
 }
 
 group = "no.nav.syfo"
